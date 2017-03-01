331dabc312035294a33b0bcd51684535a996db15
kassadube <noamsrb@yahoo.com>
2017-03-01 15:49:02 +0200

    add not found page

--- client-my/src/index.js
+++ client-my/src/index.js
@@ -9,6 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // our packages
 import Home from './pages/home';
+import NotFound from './pages/notFound';
 import App from './app';
 import store from './store';
 
@@ -20,6 +21,8 @@ ReactDOM.render(
     <Router history={history}>
       <Route path="/" component={App}>
         <IndexRoute component={Home} />
+        <Route path="home" component={Home} />
+        <Route path="*" component={NotFound} />
       </Route>
     </Router>
   </Provider>
