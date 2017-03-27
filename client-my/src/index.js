d6ec342d4c8eb56c001fe9b41f4a1212a19d3334
kassadube <noamsrb@yahoo.com>
2017-03-27 23:21:22 +0300

    run rethink from node

--- client-my/src/index.js
+++ client-my/src/index.js
@@ -33,3 +33,18 @@ ReactDOM.render(
     </Router>
   </Provider>
 , document.getElementById('app'));
+
+/*
+var child = require('child_process').execFile;
+// var executablePath = "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe";
+var executablePath  = "C:\\Users\\noam\\RethinkDB\\rethinkdb.exe";
+
+child(executablePath, function(err, data) {
+    if(err){
+       console.error(err);
+       return;
+    }
+ 
+    console.log(data.toString());
+});
+*/
