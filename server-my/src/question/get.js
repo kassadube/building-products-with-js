815d3d2ee24b6518a675a6361584e0d667134a1d
kassadube <noamsrb@yahoo.com>
2017-04-18 15:48:19 +0300

    merge user owner to question

--- server-my/src/question/get.js
+++ server-my/src/question/get.js
@@ -1,7 +1,7 @@
 import passport from 'passport';
 import moment from 'moment';
 
-import {Question} from '../db';
+import {r, Question, User} from '../db';
 import {asyncRequest} from '../util';
 
 export default (app) => {
@@ -16,9 +16,16 @@ export default (app) => {
 
   app.get('/api/question/', passport.authenticate('jwt', {session: false}), asyncRequest(async (req, res) => {
     try {
-      const questions = await Question.orderBy('creationDate').limit(10);
+      const questions = await Question
+      .merge(q => ({
+        owner: r.table('User').get(q('owner')).without(['password']),
+      }))
+      .orderBy('creationDate').limit(10)
+      .execute();
+      
       res.send(questions);
     } catch (e) {
+      console.log(e.toString());
       res.status(400).send({error: e.toString()});
     }
   }));
