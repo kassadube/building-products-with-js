1bd86d65211ee6d88a4cacfcd77c94f4dd79f36a
kassadube <noamsrb@yahoo.com>
2017-05-14 23:49:41 +0300

    jj

--- client-gulp/src/app/__tests__/app.test.js
+++ client-gulp/src/app/__tests__/app.test.js
@@ -3,6 +3,6 @@ import App from '../index';
 import {shallow} from 'enzyme';
 
 test('# App', () => {
-    const wrapper = shallow(<Label>Hellw</Label>)
+    const wrapper = shallow(<Label>Hellw</Label>) 
   expect(true);
 });
