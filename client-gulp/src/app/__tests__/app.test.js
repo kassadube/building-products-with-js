c90ff1a37e770da79335e0147b3f456d035f05b7
kassadube <noamsrb@yahoo.com>
2017-05-15 12:25:35 +0300

    dd

--- client-gulp/src/app/__tests__/app.test.js
+++ client-gulp/src/app/__tests__/app.test.js
@@ -3,11 +3,7 @@ import App from '../index';
 import {shallow} from 'enzyme';
 
 test('# App', () => {
-<<<<<<< HEAD
   const wrapper = shallow(<App>test</App>);
   expect(wrapper).toMatchSnapshot();
-=======
-    const wrapper = shallow(<Label>Hellw</Label>) 
-  expect(true);
->>>>>>> 1bd86d65211ee6d88a4cacfcd77c94f4dd79f36a
+
 });
